e66a5d98b6a8b57341896c183f4b265bed827114
Tamrin007 <tamrin.bitterchococo@gmail.com>
2017-01-21 13:14:26 +0900

    run goagen

---
+++ hello.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"github.com/Tamrin007/goa-demo/app"
+	"github.com/goadesign/goa"
+)
+
+// HelloController implements the Hello resource.
+type HelloController struct {
+	*goa.Controller
+}
+
+// NewHelloController creates a Hello controller.
+func NewHelloController(service *goa.Service) *HelloController {
+	return &HelloController{Controller: service.NewController("HelloController")}
+}
+
+// Hello runs the hello action.
+func (c *HelloController) Hello(ctx *app.HelloHelloContext) error {
+	// HelloController_Hello: start_implement
+
+	// Put your logic here
+
+	// HelloController_Hello: end_implement
+	res := &app.GoaExampleHello{}
+	return ctx.OK(res)
+}
---
+++ main.go
@@ -0,0 +1,29 @@
+//go:generate goagen bootstrap -d github.com/Tamrin007/goa-demo/design
+
+package main
+
+import (
+	"github.com/Tamrin007/goa-demo/app"
+	"github.com/goadesign/goa"
+	"github.com/goadesign/goa/middleware"
+)
+
+func main() {
+	// Create service
+	service := goa.New("Demo App")
+
+	// Mount middleware
+	service.Use(middleware.RequestID())
+	service.Use(middleware.LogRequest(true))
+	service.Use(middleware.ErrorHandler(service, true))
+	service.Use(middleware.Recover())
+
+	// Mount "Hello" controller
+	c := NewHelloController(service)
+	app.MountHelloController(service, c)
+
+	// Start service
+	if err := service.ListenAndServe(":8080"); err != nil {
+		service.LogError("startup", "err", err)
+	}
+}
